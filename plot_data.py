ecdc85d23e030966aba047552c19436019da2590
yonmaor1 <yonmaor@cmu.edu>
2025-02-10 17:11:44 -0500

    data rate plot

--- plot_data.py
+++ plot_data.py
@@ -3,15 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV files
-los_df = pd.read_csv('data/los.csv')
-nlos_df = pd.read_csv('data/nlos.csv')
+los_df = pd.read_csv('data/los_datarate.csv')
+nlos_df = pd.read_csv('data/nlos_datarate.csv')
 
 los_df['Signal strength (dBm)'] = los_df['Signal strength (dBm)'].str.replace('dBm', '').astype(float)
 nlos_df['Signal strength (dBm)'] = nlos_df['Signal strength (dBm)'].str.replace('dBm', '').astype(float)
+los_df['Data rate'] = los_df['Data rate'].astype(float)
+nlos_df['Data rate'] = nlos_df['Data rate'].astype(float)
 
 plt.figure(figsize=(10, 6))
 plt.plot(los_df['No.'], los_df['Signal strength (dBm)'], label='LOS', alpha=0.5)
 plt.plot(nlos_df['No.'], nlos_df['Signal strength (dBm)'], label='NLOS', alpha=0.5)
+# plt.plot(los_df['No.'], los_df['Data rate'], label='LOS Data Rate', alpha=0.5)
+# plt.plot(nlos_df['No.'], nlos_df['Data rate'], label='NLOS Data Rate', alpha=0.5)
 
 plt.xlabel('Number')
 plt.ylabel('Signal Strength')
@@ -20,9 +24,13 @@ plt.title('Signal Strength vs Number')
 # moving averages
 los_df['Moving Average'] = los_df['Signal strength (dBm)'].rolling(window=50).mean()
 nlos_df['Moving Average'] = nlos_df['Signal strength (dBm)'].rolling(window=50).mean()
+# los_df['Data Rate Moving Average'] = los_df['Data rate'].rolling(window=50).mean()
+# nlos_df['Data Rate Moving Average'] = nlos_df['Data rate'].rolling(window=50).mean()
 
 plt.plot(los_df['No.'], los_df['Moving Average'], label='LOS Moving Average', linestyle='--')
 plt.plot(nlos_df['No.'], nlos_df['Moving Average'], label='NLOS Moving Average', linestyle='--')
+# plt.plot(los_df['No.'], los_df['Data Rate Moving Average'], label='LOS Data Rate Moving Average', linestyle='--')
+# plt.plot(nlos_df['No.'], nlos_df['Data Rate Moving Average'], label='NLOS Data Rate Moving Average', linestyle='--')
 
 plt.legend()
 
