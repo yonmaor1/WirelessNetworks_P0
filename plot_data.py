0a1edee68435f10731e164a0945c5f153fc7a94b
yonmaor1 <yonmaor@cmu.edu>
2025-02-16 15:54:18 -0500

    yon's data + graphs

--- plot_data.py
+++ plot_data.py
@@ -3,36 +3,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV files
-los_df = pd.read_csv('data/los_datarate.csv')
-nlos_df = pd.read_csv('data/nlos_datarate.csv')
+los_df = pd.read_csv('data/yon_los.csv')
+nlos_cardboard_df = pd.read_csv('data/yon_nlos_cardboard.csv')
+nlos_wood_df = pd.read_csv('data/yon_nlos_wood.csv')
+nlos_steel_df = pd.read_csv('data/yon_nlos_steel.csv')
 
-los_df['Signal strength (dBm)'] = los_df['Signal strength (dBm)'].str.replace('dBm', '').astype(float)
-nlos_df['Signal strength (dBm)'] = nlos_df['Signal strength (dBm)'].str.replace('dBm', '').astype(float)
-los_df['Data rate'] = los_df['Data rate'].astype(float)
-nlos_df['Data rate'] = nlos_df['Data rate'].astype(float)
+df_labels = ['LOS', 'NLOS (cardboard)', 'NLOS (wood)', 'NLOS (steel)']
+dfs = [los_df, nlos_cardboard_df, nlos_wood_df, nlos_steel_df]
 
+# clean the data
+for df in dfs:
+    df['No.'] = df['No.'].astype(int)
+    df['Signal strength (dBm)'] = df['Signal strength (dBm)'].str.replace('dBm', '').astype(float)
+    df['Time'] = df['Time'].astype(float)
+    df['Distance'] = df['Time'] / 5 * 0.3 # meters
+    df['Data rate (Mb/s)'] = df['Data rate (Mb/s)'].astype(float)
+
+    df = df[df['Time'] <= 10]
+
+# plot signal strength
 plt.figure(figsize=(10, 6))
-plt.plot(los_df['No.'], los_df['Signal strength (dBm)'], label='LOS', alpha=0.5)
-plt.plot(nlos_df['No.'], nlos_df['Signal strength (dBm)'], label='NLOS', alpha=0.5)
-# plt.plot(los_df['No.'], los_df['Data rate'], label='LOS Data Rate', alpha=0.5)
-# plt.plot(nlos_df['No.'], nlos_df['Data rate'], label='NLOS Data Rate', alpha=0.5)
+for df, label in zip(dfs, df_labels):
+    # raw data
+    plt.plot(df['Distance'], df['Signal strength (dBm)'], label=label, alpha=0.1)
+    # moving averages
+    df['Moving Average'] = df['Signal strength (dBm)'].rolling(window=100).mean()
+    plt.plot(df['Distance'], df['Moving Average'], label=f'{label} Moving Average', linestyle='--')
 
-plt.xlabel('Number')
-plt.ylabel('Signal Strength')
-plt.title('Signal Strength vs Number')
-
-# moving averages
-los_df['Moving Average'] = los_df['Signal strength (dBm)'].rolling(window=50).mean()
-nlos_df['Moving Average'] = nlos_df['Signal strength (dBm)'].rolling(window=50).mean()
-# los_df['Data Rate Moving Average'] = los_df['Data rate'].rolling(window=50).mean()
-# nlos_df['Data Rate Moving Average'] = nlos_df['Data rate'].rolling(window=50).mean()
-
-plt.plot(los_df['No.'], los_df['Moving Average'], label='LOS Moving Average', linestyle='--')
-plt.plot(nlos_df['No.'], nlos_df['Moving Average'], label='NLOS Moving Average', linestyle='--')
-# plt.plot(los_df['No.'], los_df['Data Rate Moving Average'], label='LOS Data Rate Moving Average', linestyle='--')
-# plt.plot(nlos_df['No.'], nlos_df['Data Rate Moving Average'], label='NLOS Data Rate Moving Average', linestyle='--')
-
+plt.xlabel('Distance (m)')
+plt.ylabel('Signal Strength (dBm)')
+plt.title('Signal Strength vs Distance')
 plt.legend()
+plt.grid(True)
+plt.show()
 
+# plot data rate
+plt.figure(figsize=(10, 6))
+for df, label in zip(dfs, df_labels):
+    # raw data
+    plt.plot(df['Distance'], df['Data rate (Mb/s)'], label=label, alpha=0.1)
+    # moving averages
+    df['Data Rate Moving Average'] = df['Data rate (Mb/s)'].rolling(window=100).mean()
+    plt.plot(df['Distance'], df['Data Rate Moving Average'], label=f'{label} Data Rate Moving Average', linestyle='--')
+
+plt.xlabel('Distance (m)')
+plt.ylabel('Data Rate (Mb/s)')
+plt.title('Data Rate vs Distance')
+plt.legend()
 plt.grid(True)
 plt.show()
